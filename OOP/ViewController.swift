5b191ced37712babb3b843be16b17f5f579d9584
Tommy Ryanto <tommyryanto@icloud.com>
2019-05-15 08:14:14 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Tommy Ryanto on 15/05/19.
+//  Copyright © 2019 Tommy Ryanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
