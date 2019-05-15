e6d712fba151aba308b77f435b4336da5c888fbd
Tommy Ryanto <tommyryanto@icloud.com>
2019-05-15 08:28:28 +0700

    Add a new function for sum

--- OOP/ViewController.swift
+++ OOP/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func sum(){
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("This is my first update")
+        print("This is my second update")
     }
-
 
 }
 
